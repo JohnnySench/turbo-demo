80fb17f51f742150cfb9884f7c0ec23025580e68
Evgeny Senchukov <evgeny.senchukov@lamoda.ru>
2026-05-18 22:56:19 +0300

    change config test 2

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,26 +1,23 @@
 import { defineConfig } from '@playwright/test'
 
 export default defineConfig({
-    testDir: './apps/**/e2e',
+    testDir: './apps',
 
     projects: [
         {
             name: 'base',
-            use: {
-                baseURL: 'http://localhost:4173'
-            }
+            testMatch: '**/base/e2e/*.spec.ts',
+            use: { baseURL: 'http://localhost:4173' }
         },
         {
             name: 'admin',
-            use: {
-                baseURL: 'http://localhost:4174'
-            }
+            testMatch: '**/admin/e2e/*.spec.ts',
+            use: { baseURL: 'http://localhost:4174' }
         },
         {
             name: 'promotion',
-            use: {
-                baseURL: 'http://localhost:4175'
-            }
+            testMatch: '**/promotion/e2e/*.spec.ts',
+            use: { baseURL: 'http://localhost:4175' }
         }
     ],
 
