f283600bb853cee7a45d44606b6e10c26e6554fe
Evgeny Senchukov <evgeny.senchukov@lamoda.ru>
2026-05-18 22:07:51 +0300

    change CI 2

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,6 +3,27 @@ import { defineConfig } from '@playwright/test'
 export default defineConfig({
     testDir: './apps/**/e2e',
 
+    projects: [
+        {
+            name: 'base',
+            use: {
+                baseURL: 'http://localhost:4173'
+            }
+        },
+        {
+            name: 'admin',
+            use: {
+                baseURL: 'http://localhost:4174'
+            }
+        },
+        {
+            name: 'promotion',
+            use: {
+                baseURL: 'http://localhost:4175'
+            }
+        }
+    ],
+
     use: {
         headless: true
     }
