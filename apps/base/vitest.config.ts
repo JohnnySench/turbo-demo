a9a7d02cd4c3bbdfe095790c1cb4724adff4ad85
Evgeny Senchukov <evgeny.senchukov@lamoda.ru>
2026-05-18 21:47:14 +0300

    add e2e 4

--- apps/base/vitest.config.ts
+++ apps/base/vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
     test: {
-        include: ['src/**/*.{test,spec}.ts'],
+        include: ['tests/*.{test,spec}.ts'],
         exclude: ['e2e/**']   // ← ВАЖНО
     }
 })
