43bfe4ff427efb584abd1425311cc918259cd0c1
Evgeny Senchukov <evgeny.senchukov@lamoda.ru>
2026-05-19 17:27:01 +0300

    PR-3 some changes

--- apps/promotion/e2e/promotion.spec.ts
+++ apps/promotion/e2e/promotion.spec.ts
@@ -1,4 +1,6 @@
-import { test, expect } from '@playwright/test'
+import { test } from '@playwright/test'
+
+test.use({baseURL: 'http://localhost:4175/promotion'})
 
 test('promotion loads', async ({ page }) => {
     await page.goto('/')
